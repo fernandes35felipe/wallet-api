8961e979fae0b6ca447f87c9c307e9e3f7204095
Felipe <fernandes35felipe@gmail.com>
2025-07-03 23:28:00 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://wallet-app-gray.vercel.app',
+    origin: ['https://wallet-app-gray.vercel.app', 'http://localhost:3001'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,

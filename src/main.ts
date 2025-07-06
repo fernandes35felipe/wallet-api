8194761608c99a498d164c7201a1d5b76dc8e4f9
Felipe <fernandes35felipe@gmail.com>
2025-07-06 14:56:22 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-    credentials: true,
-  });
+  const configService = app.get(ConfigService);
+  app.enableCors();
 
   await app.listen(3000);
 }
